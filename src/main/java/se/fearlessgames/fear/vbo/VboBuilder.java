7430401e3340aad583372a9f1e522015a1f3d49b
Dema <magnus2468@gmail.com>
2012-01-25 17:31:23 +0100

    VBO now uses interleaved buffer structure

--- src/main/java/se/fearlessgames/fear/vbo/VboBuilder.java
+++ src/main/java/se/fearlessgames/fear/vbo/VboBuilder.java
@@ -6,68 +6,149 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 public class VboBuilder {
-	private final FloatBuffer vertices;
-	private IntBuffer indices;
-	private VertexDrawMode drawMode = VertexDrawMode.TRIANGLES;
+    private final FloatBuffer vertices;
+    private FloatBuffer normals = FloatBuffer.allocate(0);
+    private FloatBuffer colors = FloatBuffer.allocate(0);
+    private FloatBuffer textureCoords = FloatBuffer.allocate(0);
 
-	private VboBuilder(FloatBuffer vertices) {
-		this.vertices = vertices;
-	}
+    private IntBuffer indices;
+    private VertexDrawMode drawMode = VertexDrawMode.TRIANGLES;
 
-	public static VboBuilder fromBuffer(FloatBuffer vertices) {
-		return new VboBuilder(vertices);
-	}
+    private VboBuilder(FloatBuffer vertices) {
+        this.vertices = vertices;
+        this.vertices.rewind();
+    }
 
-	public static VboBuilder fromArray(float[] vertices) {
-		return new VboBuilder(createFloatBuffer(vertices));
-	}
+    public static VboBuilder fromBuffer(FloatBuffer vertices) {
+        return new VboBuilder(vertices.duplicate());
+    }
 
-	public VboBuilder indices(IntBuffer indices) {
-		this.indices = indices;
-		return this;
-	}
+    public static VboBuilder fromArray(float[] vertices) {
+        return new VboBuilder(createFloatBuffer(vertices));
+    }
 
-	public VboBuilder indices(int[] indices) {
-		return indices(createIntBuffer(indices));
-	}
+    public VboBuilder normals(float[] normals) {
+        this.normals = createFloatBuffer(normals);
+        return this;
+    }
 
-	public VboBuilder triangles() {
-		drawMode = VertexDrawMode.TRIANGLES;
-		return this;
-	}
+    public VboBuilder normals(FloatBuffer normals) {
+        this.normals = normals.duplicate();
+        this.normals.rewind();
+        return this;
+    }
 
-	public VboBuilder quads() {
-		drawMode = VertexDrawMode.QUADS;
-		return this;
-	}
+    public VboBuilder colors(float[] colors) {
+        this.colors = createFloatBuffer(colors);
+        return this;
+    }
 
-	public VboBuilder triangleStrips() {
-		drawMode = VertexDrawMode.TRIANGLE_STRIP;
-		return this;
-	}
+    public VboBuilder colors(FloatBuffer colors) {
+        this.colors = colors.duplicate();
+        this.colors.rewind();
+        return this;
+    }
 
-	public VertexBufferObject build() {
-		if (indices == null) {
-			int[] idx = new int[vertices.capacity()];
-			for (int i = 0; i < idx.length; i++) {
-				idx[i] = i;
-			}
-			indices = createIntBuffer(idx);
-		}
-		return new VertexBufferObject(vertices, indices, drawMode);
-	}
+    public VboBuilder textureCoords(float[] textureCoords) {
+        this.textureCoords = createFloatBuffer(textureCoords);
+        return this;
+    }
 
-	static IntBuffer createIntBuffer(int[] indices) {
-		IntBuffer indexBuffer = BufferUtils.createIntBuffer(indices.length);
-		indexBuffer.put(indices);
-		indexBuffer.flip();
-		return indexBuffer;
-	}
+    public VboBuilder textureCoords(FloatBuffer textureCoords) {
+        this.textureCoords = textureCoords.duplicate();
+        this.textureCoords.rewind();
+        return this;
+    }
 
-	static FloatBuffer createFloatBuffer(float[] vertices) {
-		FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertices.length * 3);
-		vertexBuffer.put(vertices);
-		vertexBuffer.flip();
-		return vertexBuffer;
-	}
+    public VboBuilder indices(IntBuffer indices) {
+        this.indices = indices;
+        return this;
+    }
+
+    public VboBuilder indices(int[] indices) {
+        return indices(createIntBuffer(indices));
+    }
+
+    public VboBuilder triangles() {
+        drawMode = VertexDrawMode.TRIANGLES;
+        return this;
+    }
+
+    public VboBuilder quads() {
+        drawMode = VertexDrawMode.QUADS;
+        return this;
+    }
+
+    public VboBuilder triangleStrips() {
+        drawMode = VertexDrawMode.TRIANGLE_STRIP;
+        return this;
+    }
+
+    public VertexBufferObject build() {
+
+        int verticsSize = vertices.limit();
+        int normalsSize = normals.limit();
+        int colorsSize = colors.limit();
+        int texturesSize = textureCoords.limit();
+
+        FloatBuffer buffer;
+
+        if (normalsSize == 0 && colorsSize == 0 && texturesSize == 0) {
+            buffer = vertices;
+        } else {
+
+            //using V[xyz]N[xyz]C[rgba]T1[st] format for the interleaved buffer
+            buffer = BufferUtils.createFloatBuffer(verticsSize + normalsSize + colorsSize + texturesSize);
+            for (int i = 0; i < verticsSize / 3; i++) {
+                buffer.put(vertices.get()); //x
+                buffer.put(vertices.get()); //y
+                buffer.put(vertices.get()); //z
+
+                if (normalsSize != 0) {
+                    buffer.put(normals.get()); //x
+                    buffer.put(normals.get()); //y
+                    buffer.put(normals.get()); //z
+                }
+
+                if (colorsSize != 0) {
+                    buffer.put(colors.get()); //r
+                    buffer.put(colors.get()); //g
+                    buffer.put(colors.get()); //b
+                    buffer.put(colors.get()); //a
+                }
+
+                if (texturesSize != 0) {
+                    buffer.put(textureCoords.get()); //s
+                    buffer.put(textureCoords.get()); //t
+                }
+            }
+            buffer.flip();
+        }
+
+        VertexBufferObject.InterleavedBuffer interleavedBuffer = new VertexBufferObject.InterleavedBuffer(buffer, normalsSize != 0, colorsSize != 0, texturesSize != 0);
+
+        if (indices == null) {
+            int[] idx = new int[vertices.limit()];
+            for (int i = 0; i < idx.length; i++) {
+                idx[i] = i;
+            }
+            indices = createIntBuffer(idx);
+        }
+
+        return new VertexBufferObject(interleavedBuffer, indices, drawMode);
+    }
+
+    static IntBuffer createIntBuffer(int[] indices) {
+        IntBuffer indexBuffer = BufferUtils.createIntBuffer(indices.length);
+        indexBuffer.put(indices);
+        indexBuffer.flip();
+        return indexBuffer;
+    }
+
+    static FloatBuffer createFloatBuffer(float[] vertices) {
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertices.length * 3);
+        vertexBuffer.put(vertices);
+        vertexBuffer.flip();
+        return vertexBuffer;
+    }
 }
--- src/main/java/se/fearlessgames/fear/vbo/VertexBufferObject.java
+++ src/main/java/se/fearlessgames/fear/vbo/VertexBufferObject.java
@@ -8,40 +8,149 @@ import java.nio.IntBuffer;
 
 public class VertexBufferObject {
 
-	private final int vertexBufferId;
-	private final int indexBufferId;
-	private final FloatBuffer vertices;
-	private final IntBuffer indices;
-	private final VertexDrawMode drawMode;
+    private final int vertexBufferId;
+    private final int indexBufferId;
+    private final InterleavedBuffer interleavedBuffer;
+
+    private final IntBuffer indices;
+    private final VertexDrawMode drawMode;
+
+    public VertexBufferObject(InterleavedBuffer interleavedBuffer, IntBuffer indices, VertexDrawMode drawMode) {
+        this.interleavedBuffer = interleavedBuffer;
+        this.indices = indices;
+        this.drawMode = drawMode;
+
+        vertexBufferId = GL15.glGenBuffers();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, interleavedBuffer.getBuffer(), GL15.GL_STATIC_DRAW);
+
+        indexBufferId = GL15.glGenBuffers();
+
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+        GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indices, GL15.GL_STATIC_DRAW);
+
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, GL11.GL_NONE);
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, GL11.GL_NONE);
+    }
 
 
-	public VertexBufferObject(FloatBuffer vertices, IntBuffer indices, VertexDrawMode drawMode) {
-		this.vertices = vertices;
-		this.indices = indices;
-		this.drawMode = drawMode;
-		vertexBufferId = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertices, GL15.GL_STATIC_DRAW);
+    public void draw() {
+        enableStates();
 
-		indexBufferId = GL15.glGenBuffers();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
 
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
-		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indices, GL15.GL_STATIC_DRAW);
+        int stride = interleavedBuffer.getStride();
+        int offset = 0;
 
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, GL11.GL_NONE);
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, GL11.GL_NONE);
-	}
+        GL11.glVertexPointer(3, GL11.GL_FLOAT, stride, offset);
+        offset = 3 * 4;
+
+        if (interleavedBuffer.isNormals()) {
+            GL11.glNormalPointer(GL11.GL_FLOAT, stride, offset);
+            offset += (3 * 4);
+        }
+
+        if (interleavedBuffer.isColors()) {
+            GL11.glColorPointer(4, GL11.GL_FLOAT, stride, offset);
+            offset += (4 * 4);
+        }
+
+        if (interleavedBuffer.isTextureCords()) {
+            GL11.glTexCoordPointer(2, GL11.GL_FLOAT, stride, offset);
+        }
+
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+
+        GL11.glDrawElements(drawMode.getGlMode(), indices.limit(), GL11.GL_UNSIGNED_INT, 0);
 
 
-	public void draw() {
-		GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
-		GL11.glVertexPointer(3, GL11.GL_FLOAT, 0, 0);
+        disableStates();
+    }
 
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
 
-		GL11.glDrawElements(drawMode.getGlMode(), vertices.capacity(), GL11.GL_UNSIGNED_INT, 0);
+    private void enableStates() {
+        GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
 
-		GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
-	}
+        if (interleavedBuffer.isNormals()) {
+            GL11.glEnableClientState(GL11.GL_NORMAL_ARRAY);
+        }
+
+        if (interleavedBuffer.isColors()) {
+            GL11.glEnableClientState(GL11.GL_COLOR_ARRAY);
+        }
+
+        if (interleavedBuffer.isTextureCords()) {
+            GL11.glEnableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
+        }
+    }
+
+    private void disableStates() {
+        if (interleavedBuffer.isNormals()) {
+            GL11.glDisableClientState(GL11.GL_NORMAL_ARRAY);
+        }
+
+        if (interleavedBuffer.isColors()) {
+            GL11.glDisableClientState(GL11.GL_COLOR_ARRAY);
+        }
+
+        if (interleavedBuffer.isTextureCords()) {
+            GL11.glDisableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
+        }
+        GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
+    }
+
+    protected static class InterleavedBuffer {
+        private final FloatBuffer buffer; //V[xyz]N[xyz]C[rgba]T1[st]
+        private final boolean normals;
+        private final boolean colors;
+        private final boolean textureCords;
+
+        private final int stride;
+
+        public InterleavedBuffer(FloatBuffer buffer, boolean normals, boolean colors, boolean textureCords) {
+            this.buffer = buffer;
+            this.normals = normals;
+            this.colors = colors;
+            this.textureCords = textureCords;
+
+            int stride = 3; //xyz
+
+            if (normals) {
+                stride += 3; //xyz
+            }
+
+            if (colors) {
+                stride += 4; //rgba
+            }
+
+            if (textureCords) {
+                stride += 2; //st
+            }
+
+            stride *= 4; //4bytes for a float
+
+            this.stride = stride;
+
+        }
+
+        public FloatBuffer getBuffer() {
+            return buffer;
+        }
+
+        public boolean isNormals() {
+            return normals;
+        }
+
+        public boolean isTextureCords() {
+            return textureCords;
+        }
+
+        public boolean isColors() {
+            return colors;
+        }
+
+        public int getStride() {
+            return stride;
+        }
+    }
 }
