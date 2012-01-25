15d2779c6428c06b9785021c97be28db452bfabb
hiflyer <per.malmen@gmail.com>
2012-01-25 14:25:45 +0100

    Added more sides to the box

--- src/main/java/se/fearlessgames/fear/VboBox.java
+++ src/main/java/se/fearlessgames/fear/VboBox.java
@@ -16,95 +16,105 @@ import java.nio.IntBuffer;
 public class VboBox {
 
 
-    private int vertexBufferId;
-    private int indexBufferId;
+	private int vertexBufferId;
+	private int indexBufferId;
 
-    private int shaderProgram;
-    private double angle;
+	private int shaderProgram;
+	private double angle;
 
-    public VboBox() {
-        createShaders();
-        createVbo();
-    }
+	public VboBox() {
+		createShaders();
+		createVbo();
+	}
 
-    private void createShaders() {
-        Shaders shaders = new Shaders();
+	private void createShaders() {
+		Shaders shaders = new Shaders();
 
-        shaderProgram = shaders.createProgram();
-        int vertexShader = shaders.loadAndCompileVertexShader("src/main/resources/shaders/screen.vert");
-        int fragmentShader = shaders.loadAndCompileFragmentShader("src/main/resources/shaders/screen.frag");
-        shaders.attachToProgram(shaderProgram, vertexShader, fragmentShader);
+		shaderProgram = shaders.createProgram();
+		int vertexShader = shaders.loadAndCompileVertexShader("src/main/resources/shaders/screen.vert");
+		int fragmentShader = shaders.loadAndCompileFragmentShader("src/main/resources/shaders/screen.frag");
+		shaders.attachToProgram(shaderProgram, vertexShader, fragmentShader);
 
-    }
+	}
 
-    private void createVbo() {
+	private void createVbo() {
 
-        vertexBufferId = GL15.glGenBuffers();
-        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(4 * 3);
-        vertexBuffer.put(new float[]{
-                // Front face (facing viewer), correct winding order.
-                0.0f, 0.0f, 0.0f,
-                1.0f, 0.0f, 0.0f,
-                1.0f, 1.0f, 0.0f,
-                0.0f, 1.0f, 0.0f
-        });
-        vertexBuffer.flip();
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertexBuffer, GL15.GL_STATIC_DRAW);
+		vertexBufferId = GL15.glGenBuffers();
+		float[] data = {
+				// Front face (facing viewer), correct winding order.
+				-1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,
+				1.0f, 1.0f, -1.0f,
+				-1.0f, 1.0f, -1.0f,
 
-
-        indexBufferId = GL15.glGenBuffers();
-        IntBuffer indexBuffer = BufferUtils.createIntBuffer(4);
-        indexBuffer.put(new int[]{
-                0, 1, 2, 3
-        });
-        indexBuffer.flip();
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
-        GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBuffer, GL15.GL_STATIC_DRAW);
-
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, GL11.GL_NONE);
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, GL11.GL_NONE);
+				-1.0f, -1.0f, 1.0f,
+				1.0f, -1.0f, 1.0f,
+				1.0f, 1.0f, 1.0f,
+				-1.0f, 1.0f, 1.0f
+		};
+		FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(data.length * 3);
+		vertexBuffer.put(data);
+		vertexBuffer.flip();
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertexBuffer, GL15.GL_STATIC_DRAW);
 
 
-    }
+		indexBufferId = GL15.glGenBuffers();
+		int[] indices = {
+				0, 1, 2, 3,
+				7, 6, 5, 4,
+				1, 5, 6, 2,
+				0, 3, 7, 4
+		};
+		IntBuffer indexBuffer = BufferUtils.createIntBuffer(indices.length);
+		indexBuffer.put(indices);
+		indexBuffer.flip();
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBuffer, GL15.GL_STATIC_DRAW);
+
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, GL11.GL_NONE);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, GL11.GL_NONE);
 
 
-    public void draw() {
-
-        GL20.glUseProgram(shaderProgram);
-
-        int pos = GL20.glGetUniformLocation(shaderProgram, "pos");
-        if (pos != -1) {
-            GL20.glUniform3f(pos, 0f, 0f, -10f);
-        } else {
-            throw new RuntimeException("Failed to get pos location");
-        }
-
-        int rot = GL20.glGetUniformLocation(shaderProgram, "rot");
-        if (rot != -1) {
-            GL20.glUniform3f(rot, (float) angle, (float) angle * 0.2f, (float) angle * 0.5f);
-        } else {
-            throw new RuntimeException("Failed to get rot location");
-        }
+	}
 
 
-        GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
-        GL11.glVertexPointer(3, GL11.GL_FLOAT, 0, 0);
+	public void draw() {
 
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+		GL20.glUseProgram(shaderProgram);
 
-        GL11.glDrawElements(GL11.GL_QUADS, 12, GL11.GL_UNSIGNED_INT, 0);
+		int pos = GL20.glGetUniformLocation(shaderProgram, "pos");
+		if (pos != -1) {
+			GL20.glUniform3f(pos, 0f, 0f, -10f);
+		} else {
+			throw new RuntimeException("Failed to get pos location");
+		}
 
-        GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
-        GL11.glDisableClientState(GL11.GL_COLOR_ARRAY);
+		int rot = GL20.glGetUniformLocation(shaderProgram, "rot");
+		if (rot != -1) {
+			GL20.glUniform3f(rot, (float) angle, (float) angle * 0.2f, (float) angle * 0.5f);
+		} else {
+			throw new RuntimeException("Failed to get rot location");
+		}
 
-        GL20.glUseProgram(0);
-    }
 
-    public void update() {
-        angle += 0.001;
-    }
+		GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBufferId);
+		GL11.glVertexPointer(3, GL11.GL_FLOAT, 0, 0);
+
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+
+		GL11.glDrawElements(GL11.GL_QUADS, 16, GL11.GL_UNSIGNED_INT, 0);
+
+		GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
+		GL11.glDisableClientState(GL11.GL_COLOR_ARRAY);
+
+		GL20.glUseProgram(0);
+	}
+
+	public void update() {
+		angle += 0.001;
+	}
 
 
 }
