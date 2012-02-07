27da533069f077db30aef56dc0531e49afe131ca
Kristofer Karlsson <kristofer.karlsson@gmail.com>
2012-02-07 14:50:05 +0100

    Fixed broken test

--- src/test/java/se/fearlessgames/fear/FearSceneTest.java
+++ src/test/java/se/fearlessgames/fear/FearSceneTest.java
@@ -2,6 +2,7 @@ package se.fearlessgames.fear;
 
 import org.junit.Before;
 import org.junit.Test;
+import se.fearlessgames.fear.math.Matrix4;
 import se.fearlessgames.fear.math.Quaternion;
 import se.fearlessgames.fear.math.Vector3;
 
@@ -25,8 +26,7 @@ public class FearSceneTest {
 	@Test
 	public void renderSimpleNode() throws Exception {
 		scene.render(renderer);
-		final Transformation transformation = new Transformation(any(Vector3.class), any(Quaternion.class), any(Vector3.class));
-		verifyNever().on(renderer).render(any(FearMesh.class), transformation.asMatrix());
+		verifyNever().on(renderer).render(any(FearMesh.class), any(Matrix4.class));
 	}
 
 
