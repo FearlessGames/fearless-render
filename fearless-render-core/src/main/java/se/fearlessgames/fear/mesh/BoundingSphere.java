be1bd9afa99b80db13a13a52fcc1f2e9527d4a28
Kristofer Karlsson <kristofer.karlsson@gmail.com>
2012-03-15 21:55:52 +0100

    Added BoundingSphere.merge and some unit tests

--- fearless-render-core/src/main/java/se/fearlessgames/fear/mesh/BoundingSphere.java
+++ fearless-render-core/src/main/java/se/fearlessgames/fear/mesh/BoundingSphere.java
@@ -10,7 +10,21 @@ public class BoundingSphere {
 	private final double radius;
 	private final Vector3 center;
 
-	public BoundingSphere(MeshData data) {
+	public BoundingSphere(double radius, Vector3 center) {
+		this.radius = radius;
+		this.center = center;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public Vector3 getCenter() {
+		return center;
+	}
+
+    // TODO: Calculate bounding sphere better than just using gravitational center
+	public static BoundingSphere create(MeshData data) {
 		data = data.duplicate();
 
 		List<Vector3> vertexes = new ArrayList<Vector3>();
@@ -37,15 +51,60 @@ public class BoundingSphere {
 				maxRadiusSqr = radiusSqr;
 			}
 		}
-		this.center = center;
-		this.radius = (Math.sqrt(maxRadiusSqr) + radiusEpsilon - 1f);
+		return new BoundingSphere(Math.sqrt(maxRadiusSqr) + radiusEpsilon - 1f, center);
 	}
 
-	public double getRadius() {
-		return radius;
+	/**
+	 * @param other
+	 * @return the smallest bounding sphere that contains both this sphere and other.
+	 */
+	public BoundingSphere merge(BoundingSphere other) {
+		Vector3 centerDiff = other.center.subtract(center);
+
+		double centerDistance = centerDiff.length();
+		double newRadius = (centerDistance + radius + other.radius) * 0.5;
+		if (centerDistance >= radiusEpsilon) {
+			double f = (centerDistance + other.radius - radius) / (2*centerDistance);
+			if (0 <= f && f <= 1) {
+				Vector3 newCenter = center.add(centerDiff.multiply(f));
+				return new BoundingSphere(newRadius, newCenter);
+			}
+		}
+		if (radius > other.radius) {
+			return this;
+		} else {
+			return other;
+		}
 	}
 
-	public Vector3 getCenter() {
-		return center;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		BoundingSphere that = (BoundingSphere) o;
+
+		if (Double.compare(that.radius, radius) != 0) return false;
+		if (center != null ? !center.equals(that.center) : that.center != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		temp = radius != +0.0d ? Double.doubleToLongBits(radius) : 0L;
+		result = (int) (temp ^ (temp >>> 32));
+		result = 31 * result + (center != null ? center.hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "BoundingSphere{" +
+				"radius=" + radius +
+				", center=" + center +
+				'}';
 	}
 }
--- fearless-render-core/src/test/java/se/fearlessgames/fear/mesh/BoundingSphereTest.java
+++ fearless-render-core/src/test/java/se/fearlessgames/fear/mesh/BoundingSphereTest.java
@@ -1,20 +1,50 @@
 package se.fearlessgames.fear.mesh;
 
 import org.junit.Test;
+import se.fearlessgames.fear.math.Vector3;
 import se.fearlessgames.fear.shape.SphereFactory;
 
 import static org.junit.Assert.assertEquals;
 
 public class BoundingSphereTest {
+	private static final double EPSILON = 1e-6;
 
 	@Test
 	public void testCreateBoundingSphere() {
 		SphereFactory sphereFactory = new SphereFactory(100, 100, 1, SphereFactory.TextureMode.PROJECTED);
 		MeshData meshData = sphereFactory.create();
-		BoundingSphere boundingSphere = new BoundingSphere(meshData);
+		BoundingSphere boundingSphere = BoundingSphere.create(meshData);
 		assertEquals(0.0, boundingSphere.getCenter().getX(), 0.01);
 		assertEquals(0.0, boundingSphere.getCenter().getY(), 0.01);
 		assertEquals(0.0, boundingSphere.getCenter().getZ(), 0.01);
 		assertEquals(1, boundingSphere.getRadius(), 0.01);
+	}
+
+	@Test
+	public void testMergeBoundingSpheresInside() throws Exception {
+		BoundingSphere a = new BoundingSphere(2.0, new Vector3(0, 0, 0));
+		BoundingSphere b = new BoundingSphere(3.0, new Vector3(0, 0, 0));
+		BoundingSphere merged = a.merge(b);
+		assertEquals(new BoundingSphere(3.0, new Vector3(0, 0, 0)), merged);
+		assertEquals(3.0, merged.getRadius(), EPSILON);
+		assertEquals(0, merged.getCenter().distance(new Vector3(0, 0, 0)), EPSILON);
+	}
+
+	@Test
+	public void testMergeBoundingSpheresAdjacent() throws Exception {
+		BoundingSphere a = new BoundingSphere(2.0, new Vector3(0, 0, 0));
+		BoundingSphere b = new BoundingSphere(2.0, new Vector3(4.0, 0, 0));
+		BoundingSphere merged = a.merge(b);
+		assertEquals(new BoundingSphere(4.0, new Vector3(2, 0, 0)), merged);
+		assertEquals(4.0, merged.getRadius(), EPSILON);
+		assertEquals(0, merged.getCenter().distance(new Vector3(2, 0, 0)), EPSILON);
+	}
+
+	@Test
+	public void testMergeBoundingSpheresIntersecting() throws Exception {
+		BoundingSphere a = new BoundingSphere(2.0, new Vector3(2.0, 0, 0));
+		BoundingSphere b = new BoundingSphere(3.0, new Vector3(4.0, 0, 0));
+		BoundingSphere merged = a.merge(b);
+		assertEquals(new BoundingSphere(3.5, new Vector3(3.5, 0, 0)), merged);
 	}
 }
