a5bc2577e543b96f347fe32e0426b5bde71817d9
hiflyer <per.malmen@gmail.com>
2012-05-21 14:15:33 +0200

    Improved logging of float buffers

--- fearless-render-core/src/main/java/se/fearlessgames/fear/gl/LoggingFearGl.java
+++ fearless-render-core/src/main/java/se/fearlessgames/fear/gl/LoggingFearGl.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.nio.FloatBuffer;
 
 public class LoggingFearGl implements InvocationHandler {
 	private final Logger log = LoggerFactory.getLogger(getClass());
@@ -21,7 +22,7 @@ public class LoggingFearGl implements InvocationHandler {
 		StringBuilder builder = new StringBuilder(method.getName()).append("(");
 		if (args != null) {
 			for (int i = 0, argsLength = args.length; i < argsLength; i++) {
-				builder.append(args[i]);
+				appendArgument(args[i], builder);
 				if (i < argsLength - 1) {
 					builder.append(", ");
 				}
@@ -31,6 +32,27 @@ public class LoggingFearGl implements InvocationHandler {
 		log.debug(builder.toString());
 		return method.invoke(delegate, args);
 	}
+
+	private void appendArgument(Object arg, StringBuilder builder) {
+		if (arg instanceof FloatBuffer) {
+			FloatBuffer buffer = (FloatBuffer) arg;
+			appendFloatBuffer(builder, buffer);
+		} else {
+			builder.append(arg);
+		}
+	}
+
+	private void appendFloatBuffer(StringBuilder builder, FloatBuffer buffer) {
+		int position = buffer.position();
+		int limit = buffer.limit();
+		builder.append('[');
+		for (int i = position; i < limit; i++) {
+			builder.append(buffer.get(i));
+			builder.append(",");
+		}
+		builder.append(']');
+	}
+
 
 	public static FearGl create(FearGl fearGl) {
 		return (FearGl) Proxy.newProxyInstance(fearGl.getClass().getClassLoader(), new Class<?>[]{FearGl.class}, new LoggingFearGl(fearGl));
